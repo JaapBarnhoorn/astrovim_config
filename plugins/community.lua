8a11861312e93164b74c19fc87fb47c2b8136c08
Jaap Barnhoorn <jaap.barnhoorn@capgemini.com>
2023-08-25 11:04:21 +0200

    setup config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,10 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
